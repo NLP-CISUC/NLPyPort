f328d4e296e08afc5dfae7473f5d81c46b661634
jdportugal <jd_portugal@hotmail.com>
2020-07-18 11:24:42 +0100

    Bug fixes for Pypi

--- NLPyPort/__init__.py
+++ NLPyPort/__init__.py
@@ -1,9 +1,9 @@
 from NLPyPort.FullPipeline import *
 import sys
 sys.path.append("..")
-from ...NLPyPort.LemPyPort.LemFunctions import *
-from ...NLPyPort.LemPyPort.dictionary import *
-from ...NLPyPort.TokPyPort.Tokenizer import *
-from ...NLPyPort.TagPyPort.Tagger import *
-from ...NLPyPort.CRF.CRF_Teste import *
-from ...NLPyPort.text import *
+from NLPyPort.LemPyPort.LemFunctions import *
+from NLPyPort.LemPyPort.dictionary import *
+from NLPyPort.TokPyPort.Tokenizer import *
+from NLPyPort.TagPyPort.Tagger import *
+from NLPyPort.CRF.CRF_Teste import *
+from NLPyPort.text import *
